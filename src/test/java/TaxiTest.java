dc3d82c5ce79426d8c5ea3fef9759fc8301b10a3
Patrickwalsh33 <patrickwalsh33@outlook.com>
2023-12-17 20:15:49 +0000

    Taxi algorithm now intakes size

--- src/test/java/TaxiTest.java
+++ src/test/java/TaxiTest.java
@@ -5,12 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TaxiTest {
     @Test
     public void testGetTaxiDetails(){
-        Taxi test = new Taxi("08-MH-3038","Jhon Doe",4,"Toyota","medium",1);
+        Taxi test = new Taxi("08-MH-3038","Jhon Doe",4,"Toyota","medium");
         assertEquals("08-MH-3038",test.getRegistration());
         assertEquals("Jhon Doe",test.getName());
         assertEquals(4,test.getRating());
         assertEquals("Toyota",test.getBrand());
         assertEquals("medium",test.getSize());
-        assertEquals(1,test.getSeats());
     }
 }
